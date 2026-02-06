1b359f369044070cc0ad4e5f7e1786043c29136d
Ravidu Senavirathna <ravidupasan11@gmail.com>
2026-02-06 15:39:52 +0530

    added processed img preview function

--- pre_processing.py
+++ pre_processing.py
@@ -1,20 +1,38 @@
 import cv2
-
+import matplotlib.pyplot as plt
 
 # To perform pre-processing on an image, including converting to grayscale, applying Gaussian blur, and performing Canny edge detection.
 def img_processing(img):
     # convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # smooth (important for pencil)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
 
 
     # Canny edge detection
-    edges = cv2.Canny(
-        gray,
+    processed_img = cv2.Canny(
+        img,
         threshold1=60,
         threshold2=60
     )
 
-    return edges
+    return processed_img
+
+
+# To load an image, perform pre-processing, and display the Canny edge image using Matplotlib.
+def processed_img_preview(img_path):
+    # ---- load image ----
+    img = cv2.imread(img_path)
+
+    if img is None:
+        raise ValueError("Image not found")
+
+    processed_img = img_processing(img)
+
+    plt.figure(figsize=(4,4))
+    plt.title("Processed Image")
+    plt.imshow(processed_img, cmap="gray")
+    plt.axis("off")
+
+    plt.show()
