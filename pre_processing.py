326efbce23d22dcfcdb051e3700aa14e62ebe178
Ravidu Senavirathna <ravidupasan11@gmail.com>
2026-02-23 23:59:49 +0530

    tight crop

--- pre_processing.py
+++ pre_processing.py
@@ -18,6 +18,22 @@ def img_processing(img):
     return thresh
 
 
+def processed_img_crop(img):
+
+    thresh = img_processing(img)
+
+    # Find contours
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        largest_contour = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        cropped = thresh[y:y+h, x:x+w]
+    else:
+        cropped = thresh
+
+    return cropped
+
 # To load an image, perform pre-processing, and display the Canny edge image using Matplotlib.
 def processed_img_preview(img_path):
     # ---- load image ----
